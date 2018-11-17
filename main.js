0645bd115e7505a956171ea111f9614556bc37bf
Andrey <staybox@gmail.com>
2018-11-17 22:56:02 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,53 @@
+var fs = require("fs");
+var path = require("path");
+
+function check(arg1, arg2) {
+    return !(isNaN(arg1) || isNaN(arg2));
+}
+
+function listDirAndMoveFiles(dir, dest) {
+    if (!dir || !dest) {
+        return console.log("need two args");
+    }
+
+    if (!(isNaN(dir) || isNaN(dest))) {
+        return console.log("Вы ввели числа вместо указания строчных аргументов");
+    }
+
+    fs.readdir(dir, (err, files) => {
+        if (err) {
+            console.log("Ошибка чтения каталога");
+        }
+
+        // Создание новой корневой папки и проверка
+        if (!fs.existsSync(dest)) {
+            return fs.mkdirSync(dest);
+        }
+
+        files.forEach(function (file) {
+            file = path.resolve(dir, file);
+
+
+            fs.stat(file, function (err, stat) {
+                if (stat.isDirectory()) {
+                    listDirAndMoveFiles(file, dest);
+
+                } else if (!stat.isDirectory()) {
+
+                    var name = dest + "\\" + path.basename(file)[0] + "\\" + path.basename(file);
+                    var folder = dest + "\\" + path.basename(file)[0];
+                    // Создание новой корневой папки и проверка
+                    if (!fs.existsSync(folder)) {
+                        fs.mkdirSync(folder);
+                    }
+
+                    fs.renameSync(file, name);
+                }
+            });
+        });
+    });
+    return 0;
+}
+
+listDirAndMoveFiles(process.argv[2], process.argv[3]);
+
