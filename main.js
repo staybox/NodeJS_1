d684e9be703702736a64a030805b2f7b260200c8
Andrey <staybox@gmail.com>
2018-11-26 13:21:07 +0300

    Доработка

--- main.js
+++ main.js
@@ -9,13 +9,16 @@ var del = require('del');
 /**
  * Commander
  */
+
 program
   .version('1.0.0')
+  
   .option('-d, --dir [myVar]', 'Add source directory')
   .option('-e, --dest [myVar]', 'Add destination directory')
   .option('-x, --removeSource', 'Delete source directory with subfolders')
   .parse(process.argv); 
   
+
     
 var where = program.dir;
 var these = program.dest;
@@ -34,39 +37,38 @@ var obs = new observer(() => {
     
 });
 
+ 
+if (!where || !these) {
+    return console.log("need two args");
+ }
+
+if (!(isNaN(where) || isNaN(these))) {
+    return console.log("Вы ввели числа вместо указания строчных аргументов");
+}
+
+if (!fs.existsSync(these)) {
+    fs.mkdirSync(these);
+}
+
 
 function listDirAndMoveFiles(where, these) {
-    
-    if (!where || !these) {
-        return console.log("need two args");
-    }
-
-    if (!(isNaN(where) || isNaN(these))) {
-        return console.log("Вы ввели числа вместо указания строчных аргументов");
-    }
-
     
     fs.readdir(where, (err, files) => {
         if (err) {
             return console.log("Ошибка чтения каталога");
         }
 
-        // Создание новой корневой папки и проверка
-        if (!fs.existsSync(these)) {
-            return fs.mkdirSync(these);
-        }
-
         files.forEach(function (file) {
             file = path.resolve(where, file);
-            
+            obs.addObserver(file);
             fs.stat(file, function (err, stat) {
                 console.log(file);
                 if (stat.isDirectory()) {
-                
+                    obs.removeObserver(file);
                     listDirAndMoveFiles(file, these);
                 } 
                 if (!stat.isDirectory()) {
-                    obs.addObserver(file);
+                    //obs.addObserver(file);
                     var name = these + "\\" + path.basename(file)[0] + "\\" + path.basename(file);
                     var folder = these + "\\" + path.basename(file)[0];
                     // Создание новой корневой папки и проверка
